3af50b13870b264b3eda90aad972cab2df9048fc
Xi Ouyang <simonouyang@gmail.com>
2025-10-11 16:23:32 -0700

    fix: host name

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://view-transition-souyang.vercel.app/";
+  const baseUrl = "https://view-transition-souyang.vercel.app";
   
   return [
     {
